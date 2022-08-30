ee8d79e14bbc7de8fed08fbaf35d86f6adb5e1cd
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2022-08-30 11:01:47 +0200

    lucktext

---
+++ lucktext/Program.cs
@@ -0,0 +1,17 @@
+﻿string name;
+string food;
+string place;
+
+
+Console.WriteLine("Vad är ditt namn?");
+name = Console.ReadLine();
+
+Console.WriteLine("Vad är din favorit mat?");
+food = Console.ReadLine();
+
+Console.WriteLine("Vad är din favorit plats?");
+place = Console.ReadLine();
+
+Console.WriteLine($"{name} gick på en promenad till butiken och köpte {food}, sedan gick han till {place} och åt.");
+
+Console.ReadLine();
